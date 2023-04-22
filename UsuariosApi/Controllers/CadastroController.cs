4db716464f3a4a39af7bc4c1b9700e151b8b9655
Jessica Magalhaes <jfdmagalhaes@hotmail.com>
2023-04-22 14:55:37 -0300

    Added user api

---
+++ UsuariosApi/Controllers/CadastroController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+
+namespace UsuariosApi.Controllers
+{
+    [Route("[controller]")]
+    public class CadastroController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateUser(CreateUserDto createDto)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ UsuariosApi/Profiles/ProfileUser.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using UsuariosApi.Data.Dtos;
+using UsuariosApi.Models;
+
+namespace UsuariosApi.Profiles
+{
+    public class ProfileUser : Profile
+    {
+        public ProfileUser()
+        {
+            CreateMap<CreateUserDto, User>();
+        }
+    }
+}
